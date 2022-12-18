beecdab1fe2d273bc06f4bae90331082d56352b1
Abel Junior <abel.costa@webjump.com.br>
2022-12-18 01:28:15 -0300

    chore: adding code

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,6 @@ export let acquireReading = () => ({
 
 export let baseRate = (month: number, year: number) =>
   Math.floor(year / 1000) + month;
+
+export let baseCharge = (month: number, year: number, quantity: number) =>
+  baseRate(month, year) * quantity;
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,4 +1,4 @@
-import { acquireReading, baseRate } from "../src/main";
+import { acquireReading, baseRate, baseCharge } from "../src/main";
 
 describe("acquireReading", () => {
   it("should return an valid object", () => {
@@ -18,5 +18,13 @@ describe("baseRate", () => {
     let result = baseRate(5, 2022);
 
     expect(result).toEqual(7);
+  });
+});
+
+describe("baseCharge", () => {
+  it("should return 70 for quantity = 10", () => {
+    let result = baseCharge(5, 2022, 10);
+
+    expect(result).toEqual(70);
   });
 });
