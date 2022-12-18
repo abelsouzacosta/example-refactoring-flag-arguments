64e4723394cfdf67dd63dea42fa4350a30a495e2
Abel Junior <abel.costa@webjump.com.br>
2022-12-18 01:38:33 -0300

    refactor: extract function => baseCharge

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,17 @@ export let acquireReading = () => ({
 let taxThreshold = (year: number) => 5 - Math.floor(year / 1000);
 
 // todo: [refactor] extract function
-export let baseCharge = (month: number, year: number, quantity: number) =>
-  baseRate(month, year) * quantity;
-
-// todo: [refactor] extract function
 export let taxableCharge = (baseCharge: number, year: number) =>
   Math.max(0, baseCharge) - taxThreshold(year);
 
 export function baseRate(month: number, year: number): number {
   return Math.floor(year / 1000) + month;
+}
+
+export function baseCharge(
+  month: number,
+  year: number,
+  quantity: number
+): number {
+  return baseRate(month, year) * quantity;
 }
